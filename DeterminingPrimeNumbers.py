df275afec9fed7276913cdf8027255d2d77baf8d
akkik04 <81925146+akkik04@users.noreply.github.com>
2021-05-14 22:47:23 -0400

    Update DeterminingPrimeNumbers.py

--- DeterminingPrimeNumbers.py
+++ DeterminingPrimeNumbers.py
@@ -1,10 +1,14 @@
 def find_prime(number):
-
+    
+    # if-statement to determine if the number is prime.
     if (number % 2 != 0):
         return 'This is a prime number'
     
+    # returning not prime if number does not meet requirements.
     return 'This is not a prime number'
-    
+
+# code to prompt the use for a number.
 number = int(input("Enter the number you would like to check please: "))
 
+# printing the output.
 print(find_prime(number))
